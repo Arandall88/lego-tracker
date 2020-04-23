bbe1cc8015b3e2429902da723ca2ecaf6cab80df
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-23 12:03:19 -0500

    compressed js files

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,3 @@
-$(document).ready(function(){console.log("This is ready")});
+$(document).ready(function () {
+  console.log("This is ready");
+});
--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -7,3 +7,11 @@ $(document).ready(function () {
     window.location.replace(`/search/${searchTerm}`);
   });
 });
+
+$(document).ready(function () {
+  $("#lego-search-button").on("click", function (a) {
+    a.preventDefault(), console.log("search button");
+    const b = $("#lego-search-bar").val();
+    console.log(b), window.location.replace(`/search/${b}`);
+  });
+});
